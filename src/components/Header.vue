570d8ab4cceda04c84b936f9eabd3b4d7daf32d0
hseong3243 <hseong3243@gmail.com>
2024-02-02 23:32:48 +0900

    feat: 헤더 추가

---
+++ src/components/Header.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+  name: "Header"
+}
+</script>
+
+<template>
+<div class="d-flex justify-space-between border-b-md">
+  <div class="text-h3">Shout Link!</div>
+  <div class="d-flex">
+    <router-link to="/">홈</router-link>
+    <router-link to="/login">로그인</router-link>
+  </div>
+</div>
+</template>
+
+<style scoped>
+
+</style>
