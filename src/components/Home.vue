ec0f796945bdb2447704dcaeacacc1a75cecce21
hseong3243 <hseong3243@gmail.com>
2024-02-02 23:51:49 +0900

    feat: 링크 묶음 섹션 추가

---
+++ src/components/Home.vue
@@ -0,0 +1,27 @@
+<script>
+import LoginPage from "@/components/login/LoginPage.vue";
+import LinkBundleSection from "@/components/home/LinkBundleSection.vue";
+
+export default {
+  name: "Main",
+  components: {LinkBundleSection, LoginPage}
+}
+</script>
+
+<template>
+  <div class="d-flex">
+    <div>
+      <LinkBundleSection/>
+    </div>
+    <div>
+
+    </div>
+    <div>
+
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/home/LinkBundleSection.vue
@@ -0,0 +1,40 @@
+<script>
+export default {
+  name: "LinkBundleSection",
+  data() {
+    return {
+      linkBundles: [
+        {
+          linkBundleId: 1,
+          description: "기본 분류",
+          isDefault: true
+        },
+        {
+          linkBundleId: 2,
+          description: "관심있는 것들",
+          isDefault: false
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<template>
+  <div class="d-flex flex-column ga-2 mt-2">
+    <div class="d-flex ga-2">
+      <div class="text-h5">링크 묶음</div>
+      <v-btn density="compact" icon="mdi-plus"></v-btn>
+    </div>
+    <div class="d-flex flex-wrap ga-2">
+      <v-btn v-for="n in linkBundles"
+             :key="n">
+        {{ n.description }}
+      </v-btn>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
