2350a56cd49f92d21123af173779ac17999624c7
hseong3243 <hseong3243@gmail.com>
2024-02-03 23:45:35 +0900

    chore: axios 전역 설정 추가

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import {createApp} from 'vue'
 import router from "@/router/router.js";
 import App from "@/App.vue";
+import axios from "axios";
 
 // Vuetify
 import 'vuetify/styles'
@@ -9,11 +10,13 @@ import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
 import '@mdi/font/css/materialdesignicons.css'
 
-
 const vuetify = createVuetify({
   components,
   directives
 });
+
+//axios
+axios.defaults.baseURL = 'https://shoutlink.me'
 
 const app = createApp(App);
 app.use(router)
