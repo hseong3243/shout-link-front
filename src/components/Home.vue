d019e5e3be3be014952a8c472d42f1902604483a
hseong3243 <hseong3243@gmail.com>
2024-02-03 20:47:30 +0900

    feat: 최근 관심사 섹션 추가

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,25 +1,29 @@
 <script>
 import LoginPage from "@/components/login/LoginPage.vue";
 import LinkBundleSection from "@/components/home/LinkBundleSection.vue";
+import LinkSection from "@/components/home/LinkSection.vue";
+import InterestedSection from "@/components/home/InterestedSection.vue";
 
 export default {
   name: "Main",
-  components: {LinkBundleSection, LoginPage}
+  components: {InterestedSection, LinkSection, LinkBundleSection, LoginPage}
 }
 </script>
 
 <template>
-  <div class="d-flex">
-    <div>
-      <LinkBundleSection/>
-    </div>
-    <div>
-
-    </div>
-    <div>
-
-    </div>
-  </div>
+  <v-container>
+    <v-row>
+      <v-col cols="3">
+        <LinkBundleSection/>
+      </v-col>
+      <v-col>
+        <LinkSection/>
+      </v-col>
+      <v-col cols="3">
+        <InterestedSection/>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <style scoped>
