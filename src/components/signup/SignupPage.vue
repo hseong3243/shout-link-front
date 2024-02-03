50b9ac879224a133443c054caf7a0c90b75164cd
hseong3243 <hseong3243@gmail.com>
2024-02-03 23:35:18 +0900

    feat: 회원가입 페이지 추가

--- src/components/signup/SignupPage.vue
+++ src/components/signup/SignupPage.vue
@@ -1,11 +1,25 @@
 <script>
+import SignupForm from "@/components/signup/SignupForm.vue";
+
 export default {
-  name: "SignupPage"
+  name: "SignupPage",
+  components: {SignupForm}
 }
 </script>
 
 <template>
-
+  <v-container>
+    <v-row>
+      <v-col cols="6" class="mx-auto d-flex justify-center">
+        <h1>회원가입</h1>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col cols="6" class="mx-auto">
+        <SignupForm/>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <style scoped>
