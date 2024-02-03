150c9ee393c353104365a547d4ee102093c1518e
hseong3243 <hseong3243@gmail.com>
2024-02-03 23:34:16 +0900

    feat: 회원가입 폼 추가

---
+++ src/components/signup/SignupForm.vue
@@ -0,0 +1,43 @@
+<script>
+export default {
+  name: "SignupForm",
+  data() {
+    return {
+      createMemberRequest: {
+        email: "",
+        password: "",
+        nickname: "",
+      }
+    }
+  }
+}
+</script>
+
+<template>
+  <v-form>
+    <v-text-field
+        v-model="createMemberRequest.email"
+        label="이메일"
+        variant="solo">
+    </v-text-field>
+    <v-text-field
+        v-model="createMemberRequest.password"
+        label="비밀번호"
+        variant="solo">
+    </v-text-field>
+    <v-text-field
+        v-model="createMemberRequest.nickname"
+        label="닉네임"
+        variant="solo">
+    </v-text-field>
+    <v-btn
+        size="large"
+        block>
+      회원가입
+    </v-btn>
+  </v-form>
+</template>
+
+<style scoped>
+
+</style>
