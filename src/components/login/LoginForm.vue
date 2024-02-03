99263ec83646e85c620b74ff4be6ab37bbf0eacf
hseong3243 <hseong3243@gmail.com>
2024-02-03 23:21:20 +0900

    feat: 로그인 폼 추가

---
+++ src/components/login/LoginForm.vue
@@ -0,0 +1,36 @@
+<script>
+export default {
+  data() {
+    return {
+      loginRequest: {
+        email: "",
+        password: "",
+      }
+    }
+  }
+}
+</script>
+
+<template>
+  <v-form>
+    <v-text-field
+        v-model="loginRequest.email"
+        label="이메일"
+        variant="solo">
+    </v-text-field>
+    <v-text-field
+        v-model="loginRequest.password"
+        label="비밀번호"
+        variant="solo">
+    </v-text-field>
+    <v-btn
+        size="large"
+        block>
+      로그인
+    </v-btn>
+  </v-form>
+</template>
+
+<style scoped>
+
+</style>
