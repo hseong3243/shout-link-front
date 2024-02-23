97d341fddecb9802ad8ae23f156d5bf61be9fe1e
hseong3243 <hseong3243@gmail.com>
2024-02-23 22:19:22 +0900

    feat: 허브 링크 페이지 라우터 등록

---
+++ src/components/hub/hublink/HubLinkPage.vue
@@ -0,0 +1,33 @@
+<script>
+export default {
+  name: "HubLinkPage",
+  props: ['hubId']
+}
+</script>
+
+<template>
+<v-container>
+  <v-row>
+    <v-col>
+      <div class="d-flex flex-column">
+        <div class="text-h6 py-6">허브 이름</div>
+      </div>
+    </v-col>
+  </v-row>
+  <v-row>
+    <v-col cols="3">
+
+    </v-col>
+    <v-col>
+
+    </v-col>
+    <v-col cols="3">
+
+    </v-col>
+  </v-row>
+</v-container>
+</template>
+
+<style scoped>
+
+</style>
--- src/router/router.js
+++ src/router/router.js
@@ -3,12 +3,14 @@ import Home from "@/components/Home.vue";
 import LoginPage from "@/components/login/LoginPage.vue";
 import SignupPage from "@/components/signup/SignupPage.vue";
 import HubPage from "@/components/hub/HubPage.vue";
+import HubLinkPage from "@/components/hub/hublink/HubLinkPage.vue";
 
 const routes = [
   {path:'/', component: Home},
   {path:'/login', component: LoginPage},
   {path:'/signup', component: SignupPage},
-  {path:'/hub', component: HubPage}
+  {path:'/hub', component: HubPage},
+  {path:'/hub/:hubId', component: HubLinkPage, props: true}
 ]
 
 const router = createRouter({
