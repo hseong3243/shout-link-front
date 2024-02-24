4d0e9893a47d026eb4125981f69257e5c60ca099
hseong3243 <hseong3243@gmail.com>
2024-02-24 23:31:50 +0900

    feat: 허브 조회 api 호출 추가

--- src/components/hub/hublink/HubLinkPage.vue
+++ src/components/hub/hublink/HubLinkPage.vue
@@ -1,6 +1,7 @@
 <script>
 import HubLinkBundleSection from "@/components/hub/hublink/HubLinkBundleSection.vue";
 import HubLinkSection from "@/components/hub/hublink/HubLinkSection.vue";
+import api from "@/axios/index.js";
 
 export default {
   name: "HubLinkPage",
@@ -8,7 +9,29 @@ export default {
   props: {
     hubId: String,
   },
-
+  data() {
+    return {
+      dataReady: false,
+      hub: {
+        hubId: 1,
+        masterId: 1,
+        name: "허브 이름",
+        description: "허브 설명",
+        isPrivate: false,
+        masterNickname: "마스터 이름"
+      },
+    }
+  },
+  mounted() {
+    this.findHubApiCall();
+    this.dataReady = true;
+  },
+  methods: {
+    async findHubApiCall() {
+      const axiosResponse = await api.get('/api/hubs/' + this.hubId);
+      this.hub = axiosResponse.data;
+    }
+  }
 }
 </script>
 
@@ -17,7 +40,8 @@ export default {
     <v-row>
       <v-col>
         <div class="d-flex flex-column">
-          <div class="text-h6 py-6">허브 이름</div>
+          <div class="text-h6 py-6">{{ hub.name }}</div>
+          <div class="text-subtitle-1">{{ hub.description }}</div>
         </div>
       </v-col>
     </v-row>
