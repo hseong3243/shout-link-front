7dfc2e638212dc856373c771c50dc8bfc49ec595
hseong3243 <hseong3243@gmail.com>
2024-03-31 01:01:33 +0900

    feat: 도메인 검색 페이지 추가

---
+++ src/components/domain/DomainSearchPage.vue
@@ -0,0 +1,40 @@
+<script>
+import DomainSearchForm from "@/components/domain/DomainSearchForm.vue";
+import DomainListSection from "@/components/domain/DomainListSection.vue";
+import DomainSection from "@/components/domain/DomainSection.vue";
+import {useDomainStore} from "@/store/DomainStore.js";
+
+export default {
+  name: "DomainSearchPage",
+  components: {DomainSection, DomainListSection, DomainSearchForm},
+  setup() {
+    const domainStore = useDomainStore();
+    return {domainStore};
+  },
+  data() {
+    return {
+    }
+  },
+  mounted() {
+  }
+}
+</script>
+
+<template>
+  <div>
+    <v-container>
+      <v-row>
+        <v-col cols="6">
+          <DomainListSection/>
+        </v-col>
+        <v-col cols="6">
+          <DomainSection/>
+        </v-col>
+      </v-row>
+    </v-container>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
